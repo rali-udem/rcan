23da539fb6f5e71aee28120e3362065e460e9ce7
rali-udem <critter@iro.umontreal.ca>
2019-08-27 14:29:05 -0400

    Updated theme/subtheme stats with parent/child relations.

--- theme_subtheme_stats.py
+++ theme_subtheme_stats.py
@@ -28,14 +28,34 @@ def main():
 
     print("SubThemeId\tSubThemeCodename\tTotalOccurences\t", end='')
     for th_id in range(0, 26):
-        print(get_theme(th_id)['codename'] + '\t', end='')
+        print(get_theme(th_id)['codename'] + ('' if get_theme(th_id)['active'] else ' (inactif)') + '\t', end='')
     print()
 
     for sth_id in range(0, 467):
         try:
             codename = get_subtheme(sth_id)['codename'] if sth_id else 'no_subtheme'
+            if sth_id != 0 and not get_subtheme(sth_id)['active']:
+                codename += ' (inactif)'
+
             print(str(sth_id) + '\t' + codename + '\t' + str(numpy.sum(freqs[sth_id])) + '\t', end='')
             print('\t'.join([str(freqs[sth_id][th_id]) for th_id in range(0, 26)]))
+        except KeyError as e:
+            pass
+
+    print('\n\n\n')
+
+    # print child-parent matrix
+    for sth_id in range(0, 467):
+        try:
+            parent_id = get_subtheme(sth_id)['theme'] if sth_id else 0
+            if type(parent_id) == list:
+                assert len(parent_id) == 0
+                parent_id = 0
+
+            row = [0] * 27
+            row[0] = sth_id
+            row[parent_id + 1] = 1
+            print('\t'.join([str(x) for x in row]))
         except KeyError as e:
             pass
 
